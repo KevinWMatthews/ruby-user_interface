1cfd5f64cbc3b89651a4dccf72cf4dc21c9c8410
Kevin W Matthews <KevinWMatthews@gmail.com>
2016-03-10 08:30:54 -0800

    Inheritance...
    So soon?

--- src/user_interface.rb
+++ src/user_interface.rb
@@ -3,33 +3,24 @@
 require 'highline'
 
 class UserInterface
-  def initialize(stdin: $stdin, stdout: $stdout)
-  end
-
-  def entry(message)
-  end
-end
-
-class HighLineInterface
   attr_reader :stdin, :stdout
   def initialize(stdin: $stdin, stdout: $stdout)
     @stdin = stdin
     @stdout = stdout
   end
 
+  def entry(message)
+  end
+end
+
+class HighLineInterface < UserInterface
   def entry(message)
     ui_system = HighLine.new(stdin, stdout)
     ui_system.ask(message)
   end
 end
 
-class TerminalInterface
-  attr_reader :stdin, :stdout
-  def initialize(stdin: $stdin, stdout: $stdout)
-    @stdin = stdin
-    @stdout = stdout
-  end
-
+class TerminalInterface < UserInterface
   def entry(message)
     stdout.puts message
     stdin.gets.chomp
