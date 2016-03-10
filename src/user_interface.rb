767c60a7991062da76162a82418cadade067b365
Kevin W Matthews <KevinWMatthews@gmail.com>
2016-03-10 07:18:24 -0800

    Outline entry()

--- src/user_interface.rb
+++ src/user_interface.rb
@@ -1,4 +1,10 @@
 #!/usr/bin/env ruby
 
 class UserInterface
+  def initialize(stdin: $stdin, stdout: $stdout)
+  end
+
+  def entry(message)
+    "test"
+  end
 end
--- test/test_user_interface.rb
+++ test/test_user_interface.rb
@@ -5,7 +5,27 @@ require 'minitest/autorun'
 require_relative '../src/user_interface.rb'
 
 class UserInterfaceTest < MiniTest::Test
+  attr_reader :user_interface, :stdin, :stdout
+  def setup
+    @stdin = StringIO.new
+    @stdout = StringIO.new
+    @user_interface = UserInterface.new(stdin: stdin, stdout: stdout)
+  end
+
+  def add_items_to_input_stream(item_list)
+    item_list.each do |item|
+      stdin.puts item
+    end
+    stdin.rewind
+  end
+
   def test_initialize
     UserInterface.new
+  end
+
+  def test_entry
+    add_items_to_input_stream(["test"])
+    user_input = user_interface.entry("Message")
+    assert_equal("test", user_input)
   end
 end
