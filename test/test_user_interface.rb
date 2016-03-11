dd98b074d7d9c8586144d0ce21b8f5c06f2c686b
Kevin Matthews <kmatthews@cyberdata.net>
2016-03-11 15:20:38 -0800

    Extract output verification

--- test/test_user_interface.rb
+++ test/test_user_interface.rb
@@ -4,6 +4,36 @@ require 'minitest/autorun'
 require_relative '../src/user_interface.rb'
 include InterfaceFactory
 
+# Expected output should be passed as individual arguments.
+# Pass arrays using the splat ('*') operator.
+def add_items_to_input_stream(*items)
+  items.each do |item|
+    stdin.puts item
+  end
+  stdin.rewind
+end
+
+def read_output_stream
+  stdout.rewind
+  stdout.read
+end
+
+# Expected output should be passed as individual arguments.
+# Pass arrays using the splat ('*') operator.
+def verify_output(*expected_output)
+  actual_output = read_output_stream
+  expected_output.each do |expected|
+    assert actual_output.include?(expected.to_s), show_missing_output(expected, actual_output)
+  end
+end
+
+def show_missing_output(expected, actual)
+  "Expected output not found:\n" +
+  "#{expected}\n" +
+  "Actual output:\n" +
+  "#{actual}\n"
+end
+
 describe InterfaceFactory, "Sanity checks for factory" do
   describe "when passed an invalid interface" do
     it "raises an error" do
@@ -39,28 +69,6 @@ describe HighLineInterface, "Test HighLine Interface" do
   let (:user_input) { "" }
   let (:user_output) { nil }
 
-  def add_items_to_input_stream(item_list)
-    item_list.each do |item|
-      stdin.puts item
-    end
-    stdin.rewind
-  end
-
-  def read_output_stream
-    stdout.rewind
-    stdout.read
-  end
-
-  # Expected output should be passed as individual arguments.
-  # Pass arrays using the splat ('*') operator.
-  def verify_output(*expected_output)
-    actual_output = read_output_stream
-    expected_output.each do |expected|
-      puts expected
-      assert actual_output.include?(expected.to_s), "Expected output not found: #{expected}"
-    end
-  end
-
   it "can be initialized" do
     InterfaceFactory.factory(:highline)
   end
@@ -69,39 +77,34 @@ describe HighLineInterface, "Test HighLine Interface" do
     it "prints message and returns input" do
       user_input = "test"
       prompt = "Message"
-      add_items_to_input_stream([user_input])
+      add_items_to_input_stream(user_input)
 
       ui_result = user_interface.entry(prompt)
 
-      output = read_output_stream
-
       assert_equal(user_input, ui_result)
-      assert output.include?(prompt), "Unexpected output: #{output}"
+      verify_output(prompt)
     end
 
     it "can accept varying user input" do
       user_input = "not test"
       prompt = "Message"
-      add_items_to_input_stream([user_input])
+      add_items_to_input_stream(user_input)
 
       ui_result = user_interface.entry(prompt)
 
-      output = read_output_stream
       assert_equal(user_input, ui_result)
-      assert output.include?(prompt), "Unexpected output: #{output}"
+      verify_output(prompt)
     end
 
     it "can display varying messages" do
       user_input = "test"
       prompt = "Different Message"
-      add_items_to_input_stream([user_input])
+      add_items_to_input_stream(user_input)
 
       ui_result = user_interface.entry(prompt)
 
-      output = read_output_stream
-
       assert_equal(user_input, ui_result)
-      assert output.include?(prompt), "Unexpected output: #{output}"
+      verify_output(prompt)
     end
   end
 
@@ -109,41 +112,29 @@ describe HighLineInterface, "Test HighLine Interface" do
     it "prints all items and returns the user's selection" do
       user_input = 1
       prompt = "Select an item from the list"
-      add_items_to_input_stream([user_input])
+      add_items_to_input_stream(user_input)
 
       list = [:item1, :item2, :item3]
       expected = list[user_input-1]   # HighLine user entries are indexed from 1
 
       ui_result = user_interface.select_from_list(prompt, list)
 
-      # Verify method return value
       assert_equal(expected, ui_result)
-
-      # Verify output
-      verify_output(*list)
+      verify_output(prompt, *list)
     end
 
     it "sends an error message if the user selects invalid input" do
       user_input = ["invalid", 1]
       prompt = "Select an item from the list"
-      add_items_to_input_stream([user_input])
+      add_items_to_input_stream(user_input)
 
       list = [:item1, :item2, :item3]
       expected = list[1-1]   # HighLine user entries are indexed from 1
 
       ui_result = user_interface.select_from_list(prompt, list)
 
-      # Verify method return value
       assert_equal(expected, ui_result)
-
-      # Verify output
-      output = read_output_stream
-
-      assert output.include?("You must choose one of"), "Unexpected output: #{output}"
-      assert output.include?(prompt), "Unexpected output: #{output}"
-      list.each do |item|
-        assert output.include?(item.to_s)
-      end
+      verify_output(prompt, *list)
     end
   end
 end
@@ -155,18 +146,6 @@ describe TerminalInterface, "Test basic terminal user interface" do
   let (:user_input) { "" }
   let (:user_output) { nil }
 
-  def add_items_to_input_stream(item_list)
-    item_list.each do |item|
-      stdin.puts item
-    end
-    stdin.rewind
-  end
-
-  def read_output_stream
-    stdout.rewind
-    stdout.read
-  end
-
   it "can be initialized" do
     InterfaceFactory.factory(:terminal)
   end
@@ -175,39 +154,34 @@ describe TerminalInterface, "Test basic terminal user interface" do
     it "prints message and returns input" do
       user_input = "test"
       prompt = "Message"
-      add_items_to_input_stream([user_input])
+      add_items_to_input_stream(user_input)
 
       ui_result = user_interface.entry(prompt)
 
-      output = read_output_stream
-
       assert_equal(user_input, ui_result)
-      assert output.include?(prompt), "Unexpected output: #{output}"
+      verify_output(prompt)
     end
 
     it "can accept varying user input" do
       user_input = "not test"
       prompt = "Message"
-      add_items_to_input_stream([user_input])
+      add_items_to_input_stream(user_input)
 
       ui_result = user_interface.entry(prompt)
 
-      output = read_output_stream
       assert_equal(user_input, ui_result)
-      assert output.include?(prompt), "Unexpected output: #{output}"
+      verify_output(prompt)
     end
 
     it "can display varying messages" do
       user_input = "test"
       prompt = "Different Message"
-      add_items_to_input_stream([user_input])
+      add_items_to_input_stream(user_input)
 
       ui_result = user_interface.entry(prompt)
 
-      output = read_output_stream
-
       assert_equal(user_input, ui_result)
-      assert output.include?(prompt), "Unexpected output: #{output}"
+      verify_output(prompt)
     end
   end
 end
